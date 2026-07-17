7285b4e7d938ca52d594b284566c9f06f58040dd
Muhammad Anas <k250899@nu.edu.pk>
2026-07-17 10:59:16 +0500

    Time: 2 ms (14.91%), Space: 9.1 MB (14.17%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       std::stack <char> stack;
+       std::unordered_map <char,char> CloseToOpen = {
+        {')','('},
+        {'}','{'},
+        {']','['}
+       };
+       for(char c : s){
+        if(CloseToOpen.count(c)){
+             if(!stack.empty() && stack.top() == CloseToOpen[c]){
+                stack.pop();
+            }else{
+                return false;
+            }
+        }else{
+            stack.push(c);
+        }
+            
+       }
+       return stack.empty();
+    }
+};
