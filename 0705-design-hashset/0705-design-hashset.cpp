a39b434b79171db70e0c2e47a2bf9667acba1ded
Muhammad Anas <k250899@nu.edu.pk>
2026-07-18 22:19:20 +0500

    Time: 10 ms (76.41%), Space: 47.9 MB (82.22%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,20 @@
+class MyHashSet {
+    unordered_set<int> set;
+public:
+    
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        set.insert(key);
+    }
+    
+    void remove(int key) {
+        set.erase(key);
+    }
+    
+    bool contains(int key) {
+        return set.contains(key);
+    }
+};
